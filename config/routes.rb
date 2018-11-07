c3c9a14c20f7d6e06caae72cdc3b9a9fa0e0bf62
Wesley Jones <whjones@memphis.edu>
2018-11-07 19:13:37 +0000

    Started adding pages, they may need moving as models are created for use

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get 'home', to: 'pages#home', as: 'home'
+  get 'eventinfo', to: 'pages#eventinfo', as: 'eventinfo'
+  # these may need changing as models come into play
+  get 'userauth', to: 'pages#userauth', as: 'userauth'
+  get 'schedule', to: 'pages#schedule', as: 'schedule'
 end
