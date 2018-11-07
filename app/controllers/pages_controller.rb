cb48a5e9dee848c1cef97d99fd44ef2c80d0ff87
Wesley Jones <whjones@memphis.edu>
2018-11-07 19:32:22 +0000

    fixed pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,10 @@ class PagesController < ApplicationController
     def eventinfo
         #render eventinfo.html.erb
     end
-    
+    def schedule
+        #render schedule.html.erb
+    end
+    def userauth
+        #render user signup/login from userauth.html.erb
+
 end
