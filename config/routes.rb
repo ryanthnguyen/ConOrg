005ab64c6c5bd840e7c27fc4566f2a59ad4192e3
Wesley Jones <whjones@memphis.edu>
2018-11-19 08:26:44 +0000

    added admin controler and routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,44 @@ Rails.application.routes.draw do
   get 'profilepage', to: 'pages#profilepage', as: 'profilepage'
   get 'dealerinfo', to: 'pages#dealerinfo', as: 'dealerinfo'
   get 'applications', to: 'pages#applications', as: 'applications'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  get 'admins', to: 'admins#index', as: 'admins'
+  get 'admin/new', to: 'admin#new', as: 'new_admin'
+  post 'admins', to: 'admins#create'
+  get 'admins/:id/edit', to: 'admins#edit', as: 'edit_admin'
+  get 'admins/:admin_id/appraisals/new', to: 'appraisals#new', as: 'new_appraisal'
+  get 'appraiers/:id', to: 'admins#show', as: 'admin'
+  patch 'admins/:id', to: 'admins#update'
+  put 'admins/:id', to: 'admins#update'
+  delete 'admins/:id', to: 'admins#destroy'
+  get 'admins/:id/home', to: 'admins#home', as: 'admin_home'
 end
