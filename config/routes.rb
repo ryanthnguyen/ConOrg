0d8157bef3101c8b814ed20e85d52adf66e4c3c6
Wesley Jones <whjones@memphis.edu>
2018-11-19 06:12:06 +0000

    Updated routes to reflect controller change

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,15 @@ Rails.application.routes.draw do
   get 'profilepage', to: 'pages#profilepage', as: 'profilepage'
   get 'dealerinfo', to: 'pages#dealerinfo', as: 'dealerinfo'
   get 'applications', to: 'pages#applications', as: 'applications'
+
+  get 'users', to: 'users#index', as: 'users'
+  get 'user/new', to: 'user#new', as: 'new_user'
+  post 'users', to: 'users#create'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  get 'users/:user_id/appraisals/new', to: 'appraisals#new', as: 'new_appraisal'
+  get 'appraiers/:id', to: 'users#show', as: 'user'
+  patch 'users/:id', to: 'users#update'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
+  get 'users/:id/home', to: 'users#home', as: 'user_home'
 end
