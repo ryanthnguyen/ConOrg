f2c394694023a0e9a087c2ffad1b937f8db23ce6
Wesley Jones <whjones@memphis.edu>
2018-11-21 08:25:22 +0000

    fixed model generation

--- test/controllers/registration_forms_controller_test.rb
+++ test/controllers/registration_forms_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class RegistrationFormControllerTest < ActionDispatch::IntegrationTest
+class RegistrationFormsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
