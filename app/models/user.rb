023f969f109912af54d513962abc1336c0a9ff8c
Ryan Nguyen <ryanthnguyen@gmail.com>
2018-12-11 06:20:09 +0000

    added fixture and validation testing for Admin model

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,12 +40,12 @@ class User < ApplicationRecord
   has_many :panelist_forms, class_name: 'PanelistForm', foreign_key: 'user_id', inverse_of: :user, dependent: :destroy
   has_many :registration_forms, class_name: 'RegistrationForm', foreign_key: 'user_id', inverse_of: :user, dependent: :destroy
   validates :address, uniqueness: true, presence: true
-  validates :age, numericality: {only_integer: true}, presence: true
+  validates :age, numericality: {only_integer: true}
   validates :city, length: { maximum: 50 }, presence: true
   validates :email, format: { with: /(\A([a-z]*\s*)*\<*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\>*\Z)/i }, uniqueness: true, presence: true
   validates :first_name, length: { maximum: 50 }, presence: true
   validates :last_name, length: { maximum: 50 }, presence: true
-  validates :membership_type, inclusion: {in: ["Consuite Giveaway", "Banquet Tickets", "Child Pre-Regitration", "Adult Pre-Registration"]}, presence: true
+  validates :membership_type, inclusion: {in: ["Consuite Giveaway", "Banquet Tickets", "Child Pre-Regitration", "Adult Pre-Registration"]}
   validates :middle_name, length: { maximum: 50 }, presence: true
   validates :phone, numericality: {only_integer: true}, presence: true
   validates :state, inclusion: {in: %w[AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY]}, presence: true
--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -22,4 +22,62 @@ class AdminTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "valid input" do 
+    one = admins(:one)
+    assert one.valid?
+  end
+
+  test "valid first name" do
+    one = admins(:one)
+    one.first_name = ""
+    assert_not one.valid?
+  end
+
+  test "valid last name" do
+    one = admins(:one)
+    one.last_name = ""
+    assert_not one.valid?
+  end
+
+  test "valid address" do 
+    one = admins(:one) 
+    one.address = ""
+    assert_not one.valid?
+  end
+
+  test "valid city" do 
+    one = admins(:one)
+    one.city = ""
+    assert_not one.valid?
+  end
+
+  test "valid state" do 
+    one = admins(:one)
+    one.state = "BB"
+    assert_not one.valid?
+  end
+
+  test "valid zip" do
+    one = admins(:one)
+    one.zip = ""
+    assert_not one.valid?
+  end
+
+  test "valid age" do 
+    one = admins(:one)
+    one.zip = ""
+    assert_not one.valid?
+  end
+
+  test "valid email address" do 
+    one = admins(:one)
+    one.email_address = ""
+    assert_not one.valid?
+  end
+
+  test "valid phone" do 
+    one = admins(:one)
+    one.phone = ""
+    assert_not one.valid?
+  end
 end
